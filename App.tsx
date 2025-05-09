365857816848da8d4940506cc1f547b096a3686a
Alireza Joz <alireza00joz@gmail.com>
2025-05-10 01:11:17 +0330

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+// components/WelcomeCard.tsx
+import { View, Text } from 'react-native';
+import React from 'react';
+import ModuleName from './components/Container';
 
 import './global.css';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <View className="m-4 rounded-2xl bg-white p-5 shadow-lg">
+        <Text className="text-xl font-bold text-gray-800">سلام 👋</Text>
+        <Text className="mt-2 text-base text-gray-600">به اپلیکیشن NativeWind خوش اومدی!</Text>
+      </View>
+      <ModuleName />
     </>
   );
 }
--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,14 @@
-import { SafeAreaView } from 'react-native';
+// components/WelcomeCard.tsx
+import { View, Text } from 'react-native';
+import React from 'react';
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
-};
-
-const styles = {
-  container: 'flex flex-1 m-6',
-};
+export default function App() {
+  return (
+    <View className="bg-white rounded-2xl shadow-lg p-5 m-4">
+      <Text className="text-xl font-bold text-gray-800">سلام 👋</Text>
+      <Text className="text-base text-gray-600 mt-2">
+      aksmdlsak
+      </Text>
+    </View>
+  );
+}
